f6ca5521e3bb462bacecdc368dec61902f69856a
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-06-01 08:35:42 +0500

    131. Adding the Build Control Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,6 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {...}
-    // }
     state = {
        ingredients: {
            salad: 0,
