d31588e9693aaf25aa5130857f73d1fd9d148e0e
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-06-04 19:15:25 +0500

    149. Reusing the Backdrop

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,13 +5,13 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
-  <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo/>
-    <nav className={classes.DesktopOnly}>
-      <NavigationItems/>
-    </nav>
-  </header>
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <Logo/>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems/>
+        </nav>
+    </header>
 );
 
 export default toolbar;
